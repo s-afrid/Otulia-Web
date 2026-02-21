e42d65b17d6841c213e2618f1c29b110e3761b93
s-afrid <afrid0052@gmail.com>
2026-02-21 10:52:50 +0530

    increased width of details sections

--- client/src/components/asset_page/car/CarDetails.jsx
+++ client/src/components/asset_page/car/CarDetails.jsx
@@ -139,7 +139,7 @@ const CarDetails = ({ item, modelName = 'CarAsset' }) => {
   }
 
   return (
-    <div className="w-full max-w-[90%] mx-auto p-4 md:p-8 bg-white font-sans">
+    <div className="w-full max-w-[93%] mx-auto p-4 md:p-8 bg-white font-sans">
 
       <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 items-start">
 
--- client/src/components/asset_page/estate/EstateDetails.jsx
+++ client/src/components/asset_page/estate/EstateDetails.jsx
@@ -137,7 +137,7 @@ const EstateDetails = ({ item, modelName = 'EstateAsset' }) => {
   }
 
   return (
-    <div className="w-full max-w-[90%] mx-auto p-4 md:p-8 bg-white font-sans">
+    <div className="w-full max-w-[93%] mx-auto p-4 md:p-8 bg-white font-sans">
       <div className="flex flex-col lg:flex-row gap-8 lg:gap-16 items-start">
 
         {/* LEFT COLUMN: Details */}
