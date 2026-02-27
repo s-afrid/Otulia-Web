c40abc685c3b78590128843380e769279ed6a68d
s-afrid <afrid0052@gmail.com>
2026-02-27 15:38:42 +0530

    server problems

--- server/index.js
+++ server/index.js
@@ -72,6 +72,6 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on ${PORT}`)
 });
